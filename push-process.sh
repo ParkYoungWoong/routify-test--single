f207be167d4d3d8315ac9cdb0c12a13b4eedb3b0
ParkYoungWoong <thesecon@gmail.com>
2020-10-09 16:56:09 +0900

    Hello world!!

--- push-process.sh
+++ push-process.sh
@@ -1,10 +1,13 @@
 npm run build
 
 git add .
-printf "Completed git add ."
-echo -e "Input the commit message!"
+printf "파일들을 스테이지 올렸어요!\n"
+
+echo "커밋 메시지를 입력하세요!"
 read message
-git commit -m "$message"
-printf "Completed commit!"
+echo "$message"
+
+#git commit -m "$REPLY"
+#printf "Completed commit!"
 
 #git push -u origin master
